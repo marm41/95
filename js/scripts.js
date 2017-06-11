77028167b96035f02f12c19d9192e7d37fe8a7ae
Kamil Mangold <kamilmangold@gmail.com>
2017-06-11 19:26:26 +0200

    Zmiana z 4 alertów na jeden

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,5 @@ var guzik = document.getElementsByClassName('button');
 console.log(guzik);
 
 for ( var k = 0 ; k < (guzik.length) ; k++) {
-	alert(guzik[0].innerText);
-	alert(guzik[1].innerText);
-	alert(guzik[2].innerText);
-	alert(guzik[3].innerText);
-	break;
+	alert(guzik[k].innerText);
 };
